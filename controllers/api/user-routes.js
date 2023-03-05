682dd6c864a9e8d81a79be3006fb2c4516e28459
embresse <115603992+embresse@users.noreply.github.com>
2023-03-04 21:19:43 -0500

    route fix

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -5,13 +5,13 @@ const { User } = require("../../models");
 router.post("/", async (req, res) => {
   try {
     const newUser = await User.create({
-      username: req.body.email,
+      username: req.body.username,
       password: req.body.password,
     });
 
     req.session.save(() => {
       req.session.userId = newUser.id;
-      req.session.username = newUser.email;
+      req.session.username = newUser.username;
       req.session.loggedIn = true;
 
       res.json(newUser);
